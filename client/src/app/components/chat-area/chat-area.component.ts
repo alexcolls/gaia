2468a2c88b3a905c9efb9a8da9681f8beb71f762
JRosSx91 <hazee.1491@gmail.com>
2023-03-11 14:39:44 +0100

    adding events to calendar

--- client/src/app/components/chat-area/chat-area.component.ts
+++ client/src/app/components/chat-area/chat-area.component.ts
@@ -1,53 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-chat-area',
   templateUrl: './chat-area.component.html',
   styleUrls: ['./chat-area.component.css']
 })
-export class ChatAreaComponent implements OnInit {
-  public events: any[];
+export class ChatAreaComponent {
+  calendarVisible = true;
+  calendarOptions: CalendarOptions = {
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'dayGridMonth',
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  };
+  currentEvents: EventApi[] = [];
 
-  public options: any;
-  constructor() { }
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
 
-  ngOnInit() {
+  handleCalendarToggle() {
+    this.calendarVisible = !this.calendarVisible;
+  }
 
-    this.options = {
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-      defaultDate: new Date(),
-      locale: esLocale,
-      header: {
-        left: 'prev,next',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      editable: false
-    };
+  handleWeekendsToggle() {
+    const { calendarOptions } = this;
+    calendarOptions.weekends = !calendarOptions.weekends;
+  }
 
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
 
-    this.events = [
-      {
-        title: "Evento 1",
-        start: new Date().getTime(),
-        description: "evento 1"
-      },
-      {
-        title: "Evento 2",
-        start: new Date(new Date().getTime() + 86400000),
-        description: "evento 2"
-      },
-      {
-        title: "Evento 3",
-        start: new Date(new Date().getTime() + (86400000 * 2)),
-        end: new Date(new Date().getTime() + (86400000 * 3)),
-        description: "evento 2"
-      },
-    ]
+    calendarApi.unselect(); // clear date selection
 
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    this.changeDetector.detectChanges();
   }
 }
