8b910f2a515451e1c5f163a79c3814c7771a47e8
JRosSx91 <hazee.1491@gmail.com>
2023-03-16 01:25:22 +0100

    importing google auth moduls

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ErrorComponent } from './components/error/error.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { EmailsComponent } from './components/emails/emails.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -32,8 +34,27 @@ import { EmailsComponent } from './components/emails/emails.component';
     BrowserModule,
     FormsModule,
     FullCalendarModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'clientId'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
